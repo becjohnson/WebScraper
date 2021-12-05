3aefcfaecd4fbea5ddbb85955560208b151095c2
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-12-05 08:21:59 -0500

    Initial

---
+++ WebScraper/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebScraper.Models;
+
+namespace WebScraper.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string url = "https://prowritingaid.com/art/2299/positive-adjectives.aspx";
+            var response = CallUrl(url).Result;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+        private List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
+                .Where(node => !node.GetAttributeValue("class", "").Contains("marker")).ToList();
+
+            List<string> wikiLink = new List<string>();
+            foreach (var link in programmerLinks)
+            {
+                if (link.FirstChild.Attributes.Count > 0)
+                {
+                    wikiLink.Add("https://prowritingaid.com/" + link.FirstChild.Attributes[0].Value);
+                }
+            }
+            return wikiLink;
+        }
+        private void WritetoCsv(List<string> links)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var link in links)
+            {
+                sb.Append(link);
+            }
+            System.IO.File.WriteAllText("./links.csv", sb.ToString());
+        }
+    }
+}
